d8d588ebee4945a47557a53c4194d1fcd1267dcd
Laurens Mattheus <71649013+lmattheus@users.noreply.github.com>
2021-11-29 14:33:50 +0100

    auto refresh parking.py

--- Laurens/parking.py
+++ Laurens/parking.py
@@ -16,7 +16,7 @@ plaats4 = "SELECT nummerplaat FROM parkeergarage where parkeerplaats = 4 ORDER B
 print ("Content-type:text/html\n\n")
 print ("<html lang=\"en\">")
 print ("<head>")
-#print ("<meta http-equiv=\"refresh\" content=\"5\">")
+print ("<meta http-equiv=\"refresh\" content=\"5\">")
 print ("<meta charset=\"UTF-8\">")
 print ("<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css\" integrity=\"sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==\" crossorigin=\"anonymous\" referrerpolicy=\"no-referrer\" />")
 print ("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">")
